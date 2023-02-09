70be072bcd29259674b091a92afed9a4b528875c
waito3209 <60461394+waito3209@users.noreply.github.com>
2023-02-09 23:47:45 +0800

    change some var

--- linearRegression.py
+++ linearRegression.py
@@ -1,13 +1,14 @@
 import copy
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
+
+
 class Record:
-    def __init__(self, inputdata, target, perdictoutome,errorpercent:bool=True):
+    def __init__(self, inputdata, target, perdictoutome, errorpercent: bool = True):
         self.target = target
         self.perdictionoutcome = perdictoutome
         self.input = inputdata[:3]
-        self.errorinpercent=errorpercent
+        self.errorinpercent = errorpercent
         assert type(self.target) == type(self.perdictionoutcome)
         assert type(self.target) != (list or tuple or dict)
         try:
@@ -16,8 +17,8 @@ class Record:
             self.abserror = abs(self.target - self.perdictionoutcome)
             self.RM2 = self.abserror ** 2
             if self.errorinpercent:
-                self.abserror/=self.target
-                self.abserror*=100
+                self.abserror /= self.target
+                self.abserror *= 100
         except:
             try:
                 self.target = str(self.target)
@@ -30,14 +31,14 @@ class Record:
 
     def __str__(self):
         if self.errorinpercent:
-            return f"Record : Input[3]{self.input[:3]} perdict :{self.perdictionoutcome}  error in % {self.abserror }% target {self.target}"
+            return f"Record : Input[3]{self.input[:3]} perdict :{self.perdictionoutcome}  error in % {self.abserror}% target {self.target}"
 
         else:
             return f"Record : Input[3]{self.input[:3]} perdict :{self.perdictionoutcome}  abserror {self.abserror} target {self.target}"
 
 
 class Testreport():
-    def __init__(self, numberouput: bool, detail: dict = None ):
+    def __init__(self, numberouput: bool, detail: dict = None):
 
         self.numberouput = numberouput
 
@@ -58,26 +59,30 @@ class Testreport():
             self.avgRM2 = sum([x.__dict__['RM2'] for x in self.deatilrecord]) / len(self.deatilrecord)
             self.maxRM2 = max([x.__dict__['RM2'] for x in self.deatilrecord])
             self.minRM2 = min([x.__dict__['RM2'] for x in self.deatilrecord])
+
     def Drop(self):
         del self.deatilrecord
+
 
 class Trainmodelreport:
     def __init__(self):
         self.train = []
         self.val = []
 
-    def adddata(self, datatrain: Testreport=None, datatest: Testreport=None):
+    def adddata(self, datatrain: Testreport = None, datatest: Testreport = None):
         self.train.append(datatrain)
         self.val.append(datatest)
+
     def reset(self):
-        self.val=[]
-        self.train=[]
+        self.val = []
+        self.train = []
+
     def analysis(self):
-        self.trainmaxerrorlist= [x.__dict__['maxerror']  if x is not None else int(-1) for x in self.train]
+        self.trainmaxerrorlist = [x.__dict__['maxerror'] if x is not None else int(-1) for x in self.train]
         self.trainminerrorlist = [x.__dict__['minerror'] if x is not None else int(-1) for x in self.train]
         self.trainavgerrorlist = [x.__dict__['avgerror'] if x is not None else int(-1) for x in self.train]
         self.trainmaxRM2list = [x.__dict__['maxRM2'] if x is not None else int(-1) for x in self.train]
-        self.trainminRM2list= [x.__dict__['minRM2'] if x is not None else int(-1) for x in self.train]
+        self.trainminRM2list = [x.__dict__['minRM2'] if x is not None else int(-1) for x in self.train]
         self.trainavgRM2list = [x.__dict__['avgRM2'] if x is not None else int(-1) for x in self.train]
 
         self.valmaxerrorlist = [x.__dict__['maxerror'] if x is not None else int(-1) for x in self.val]
@@ -89,73 +94,74 @@ class Trainmodelreport:
 
         print(self.trainmaxerrorlist)
         print(self.valmaxerrorlist)
-        self.trainindex=[x for x in range(len(self.train))]
+        self.trainindex = [x for x in range(len(self.train))]
         self.valindex = [x for x in range(len(self.val))]
-        #print(self.train)
-        #print(len(self.train))
-        plt.plot( self.trainindex,self.trainmaxerrorlist, label='trainmaxerror')
-        plt.plot( self.trainindex,self.trainminerrorlist, label='trainminerror')
-        plt.plot(self.trainindex,self.trainavgerrorlist,  label='trainavgerror')
+        # print(self.train)
+        # print(len(self.train))
+        plt.plot(self.trainindex, self.trainmaxerrorlist, label='trainmaxerror')
+        plt.plot(self.trainindex, self.trainminerrorlist, label='trainminerror')
+        plt.plot(self.trainindex, self.trainavgerrorlist, label='trainavgerror')
         plt.ylabel('Error')
         plt.xlabel('expoch')
         plt.legend()
         plt.show()
-        plt.plot( self.trainindex, self.trainmaxRM2list,label='trainmaxRM2')
-        plt.plot( self.trainindex,self.trainminRM2list, label='trainminRM2')
-        plt.plot(self.trainindex,self.trainavgRM2list,  label='trainavgRM2')
-        plt.ylabel('Error')
-        plt.xlabel('expoch')
-        plt.legend()
-        plt.show()
-
-        plt.plot( self.valindex,self.valmaxerrorlist, label='valmaxerror')
-        plt.plot( self.valindex,self.valminerrorlist, label='valminerror')
-        plt.plot( self.valindex,self.valavgerrorlist, label='valavgerror')
-        plt.ylabel('Error')
-        plt.xlabel('expoch')
-        plt.legend()
-        plt.show()
-        plt.plot( self.valindex,self.valmaxRM2list, label='valmaxRM2')
-        plt.plot( self.valindex,self.valminRM2list, label='valminRM2')
-        plt.plot(self.valindex,self.valavgRM2list,  label='valavgRM2')
+        plt.plot(self.trainindex, self.trainmaxRM2list, label='trainmaxRM2')
+        plt.plot(self.trainindex, self.trainminRM2list, label='trainminRM2')
+        plt.plot(self.trainindex, self.trainavgRM2list, label='trainavgRM2')
         plt.ylabel('Error')
         plt.xlabel('expoch')
         plt.legend()
         plt.show()
 
-        for i in [str(x) for x in self.val[len(self.val)-1].deatilrecord]:
+        plt.plot(self.valindex, self.valmaxerrorlist, label='valmaxerror')
+        plt.plot(self.valindex, self.valminerrorlist, label='valminerror')
+        plt.plot(self.valindex, self.valavgerrorlist, label='valavgerror')
+        plt.ylabel('Error')
+        plt.xlabel('expoch')
+        plt.legend()
+        plt.show()
+        plt.plot(self.valindex, self.valmaxRM2list, label='valmaxRM2')
+        plt.plot(self.valindex, self.valminRM2list, label='valminRM2')
+        plt.plot(self.valindex, self.valavgRM2list, label='valavgRM2')
+        plt.ylabel('Error')
+        plt.xlabel('expoch')
+        plt.legend()
+        plt.show()
+
+        for i in [str(x) for x in self.val[len(self.val) - 1].deatilrecord]:
             print(i)
-        print('trainmaxerrorlist   '+str(self.trainmaxerrorlist[-1]))
-        print('trainminerrorlist   '+str(self.trainminerrorlist[-1]))
-        print('trainavgerrorlist   '+str(self.trainavgerrorlist[-1]))
-        print('trainmaxRM2list     '+str(self.trainmaxRM2list[-1]))
-        print('trainminRM2list     '+str(self.trainminRM2list[-1]))
-        print('trainavgRM2list     '+str(self.trainavgRM2list[-1]))
+        print('trainmaxerrorlist   ' + str(self.trainmaxerrorlist[-1]))
+        print('trainminerrorlist   ' + str(self.trainminerrorlist[-1]))
+        print('trainavgerrorlist   ' + str(self.trainavgerrorlist[-1]))
+        print('trainmaxRM2list     ' + str(self.trainmaxRM2list[-1]))
+        print('trainminRM2list     ' + str(self.trainminRM2list[-1]))
+        print('trainavgRM2list     ' + str(self.trainavgRM2list[-1]))
 
-        print('valmaxerrorlist     '+str(self.valmaxerrorlist[-1]))
-        print('valminerrorlist     '+str(self.valminerrorlist[-1]))
-        print('valavgerrorlist     '+str(self.valavgerrorlist[-1]))
-        print('valmaxRM2list       '+str(self.valmaxRM2list[-1]))
-        print('valminRM2list       '+str(self.valminRM2list[-1]))
-        print('valavgRM2list       '+str(self.valavgRM2list[-1]))
+        print('valmaxerrorlist     ' + str(self.valmaxerrorlist[-1]))
+        print('valminerrorlist     ' + str(self.valminerrorlist[-1]))
+        print('valavgerrorlist     ' + str(self.valavgerrorlist[-1]))
+        print('valmaxRM2list       ' + str(self.valmaxRM2list[-1]))
+        print('valminRM2list       ' + str(self.valminRM2list[-1]))
+        print('valavgRM2list       ' + str(self.valavgRM2list[-1]))
+
+
 class LinearRegression():
 
-    def __init__(self, data: list, path: str = None, constant: float = float(1),errorpercent:bool=True):
-        self.constant = constant # number of input
-        self.path = path         # output path
+    def __init__(self, data: list, path: str = None, constant: float = float(1), errorpercent: bool = True):
+        self.constant = constant  # number of input
+        self.path = path  # output path
         self.rawdata = copy.deepcopy(data)
-        self.errorpercent=errorpercent # calculate error in percentage
+        self.errorpercent = errorpercent  # calculate error in percentage
         self.traindataoutput = [float(x[-1]) for x in copy.deepcopy(data)]
-
 
         self.numberOfDimensionincludeconstant = int(len(data[0]))
         self.var = [float(0)] * self.numberOfDimensionincludeconstant
-        self.trainreport=Trainmodelreport()
+        self.trainreport = Trainmodelreport()
         for x in data:
             x[-1] = constant
         self.traindatainput = data
         self.numberofdata = len(data)
-        #print(self.rawdata)
+        # print(self.rawdata)
 
     def report(self):
 
@@ -169,7 +175,7 @@ class LinearRegression():
         return output
 
     def testonvar(self, data, showdetail=False):
-        out=Testreport(numberouput=True)
+        out = Testreport(numberouput=True)
 
         testoutput = [float(x[-1]) for x in copy.deepcopy(data)]
 
@@ -179,59 +185,61 @@ class LinearRegression():
         numberoftest = len(data)
 
         for i in range(numberoftest):
-            temprecord=Record(testinput[i],float(testoutput[i]),float(self.var_multi_input(testinput[i])),errorpercent=self.errorpercent)
+            temprecord = Record(testinput[i], float(testoutput[i]), float(self.var_multi_input(testinput[i])),
+                                errorpercent=self.errorpercent)
             out.addrecord(temprecord)
         out.analyaze()
 
         return out
 
-    def trainsimpleLR(self, epoch: int = 1, learning_rate: float = 1, learning_decay:bool = True ,
-                     learning_decay_rate:float = 0.7,
+    def trainsimpleLR(self, epoch: int = 1, learning_rate: float = 1, learning_decay: bool = True,
+                      learning_decay_rate: float = 0.7,
                       printable: bool = True, testdata: list = None,
-                      selftest: bool = True,showdot=1):
+                      selftest: bool = True, showdot=1):
         self.trainreport.reset()
 
         for epochs in range(epoch):
-            #print( self.traindataoutput)
+            # print( self.traindataoutput)
             if learning_decay:
-                a=(1/(1+epochs*learning_decay_rate))*learning_rate
+                a = (1 / (1 + epochs * learning_decay_rate)) * learning_rate
             else:
-                a=learning_rate
+                a = learning_rate
             for i in range(self.numberOfDimensionincludeconstant):
                 temp = [(self.var_multi_input(self.traindatainput[x]) - self.traindataoutput[x])
-                                    * float(self.traindatainput[x][i])
-                                    for x in range(self.numberofdata)]
-                self.var[i] -= a * sum(temp)/len(temp)
+                        * float(self.traindatainput[x][i])
+                        for x in range(self.numberofdata)]
+                self.var[i] -= a * sum(temp) / len(temp)
             if printable:
                 print(self.var)
             if showdot:
-                if epochs%int(showdot)==0:
+                if epochs % int(showdot) == 0:
                     print()
-                print('>',end='')
-            temptest=None
-            tempselftest=None
+                print('>', end='')
+            temptest = None
+            tempselftest = None
             if testdata is not None:
-                #print(testdata)
+                # print(testdata)
                 temptest = self.testonvar(copy.deepcopy(testdata))
-                if epochs!=epoch-1:
+                if epochs != epoch - 1:
                     temptest.Drop()
             if selftest:
-                tempselftest=self.testonvar(copy.deepcopy(self.rawdata.copy()))
+                tempselftest = self.testonvar(copy.deepcopy(self.rawdata.copy()))
                 if epochs != epoch - 1:
                     tempselftest.Drop()
-            self.trainreport.adddata(tempselftest,temptest)
+            self.trainreport.adddata(tempselftest, temptest)
 
     def save(self):
-        np.save(self.path,np.asarray(self.var))
+        np.save(self.path, np.asarray(self.var))
 
-def use( input:list,var):
-        output = 0
-        assert type(input) == list
-        assert len(input)+1==len(var)
-        input.append(0)
-        for i in range(len(input)):
-            output += float(var[i]) * float(input[i])
-        return output
+
+def use(input: list, var):
+    output = 0
+    assert type(input) == list
+    assert len(input) + 1 == len(var)
+    input.append(0)
+    for i in range(len(input)):
+        output += float(var[i]) * float(input[i])
+    return output
 # data = list(csv.reader(open('test.csv')))
 # test = list(csv.reader(open('val.csv')))
 # for x in data:
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from linearRegression import *
 import numpy as np
+import csv
 def Perform(name):
     data = list(csv.reader(open('traindata.csv')))   # save excel as csv dos
     test = list(csv.reader(open('testdata.csv')))
@@ -12,11 +13,13 @@ def Perform(name):
     model=LinearRegression(data,constant=1,errorpercent=False,path='vartest')
     print(model.__dict__)
 
-    model.trainsimpleLR(5000, 0.1, learning_decay=False,learning_decay_rate=0.7,
-                        testdata=test.copy(), selftest=True, showdot=0,printable=False)
+    model.trainsimpleLR(50,1.5, learning_decay=False,learning_decay_rate=0.9,
+                        testdata=test.copy(), selftest=True, showdot=1000,printable=False)
     model.save()
-    #model.report()
+    model.report()
+    print()
     print(np.load("vartest.npy"))
+    print("weight from seed : A90*3+B90*9+C90*0.23+D90+ 0 *E90")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
